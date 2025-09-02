0eca8ac2d5f29fc0cc8bc7de060ae390ee24c5a1
Douglas Graça <159181176+DougBrando@users.noreply.github.com>
2025-09-02 11:25:06 -0300

    Add files via upload

---
+++ M02S08/ComponenteNavbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+function Navbar(){
+  return(
+    <nav>
+      <ul>
+        <li><a href="#sobre">Sobre</a></li>
+        <li><a href="#beneficios">Beneficios</a></li>
+        <li><a href="#produtos">Produtos</a></li>
+        <li><a href="#contato">Contato</a></li>
+
+      </ul>
+
+      </nav>
+  )
+}
+
+export default Navbar
