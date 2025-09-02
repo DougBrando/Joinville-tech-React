abf7c951cccf3f9092ed09f5d05b9643e021f8f2
Douglas Graça <159181176+DougBrando@users.noreply.github.com>
2025-09-02 11:04:00 -0300

    Create App.jsx

---
+++ M02S08/Introducao/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div>
+    <header>
+      <h1> Energia Solar 365 </h1>
+      <h3>Economize com o sol. Invista no seu futuro!</h3>
+    </header>
+    </div>
+  )
+}
+export default App
