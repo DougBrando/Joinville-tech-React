711a61dbeda7ec63a6d63aaedd97bc4e257b67f4
Douglas Graça <159181176+DougBrando@users.noreply.github.com>
2025-09-02 11:28:29 -0300

    Add files via upload

---
+++ M02S08/ComponenteSobreNos/SobreNos.jsx
@@ -0,0 +1,26 @@
+import './SobreNos.css'
+
+
+function SobreNos() {
+  return (
+    <section id="sobre">
+      <h2>Sobre Nós</h2>
+      <figure>
+        <img src="https://www.portalsolar.com.br/_next/image?url=https%3A%2F%2Fd3csixunm0sjcw.cloudfront.net%2Fwp-content%2Fuploads%2F2021%2F05%2F25171701%2Fpainel-solar-caseiro.jpg.jpg&w=3840&q=100" />
+        <figcaption>Somos a Energia Solar 365 referência em projetos sustentáveis. Instalamos e gerenciamos sistemas residenciais, comerciais e rurais com alta eficiência e confiabilidade.</figcaption>
+      </figure>
+      <p>
+        <h3>Dados do Mercado Brasileiro (até 2025)</h3>
+        <ul>
+          <li><strong>Economia na conta de luz:</strong> até 90%</li>
+          <li><strong>Payback médio:</strong> 4 a 6 anos</li>
+          <li><strong>Redução de CO2:</strong> 1.500 kg/ano</li>
+          <li><strong>Sistemas conectados:</strong> mais de 400.000 (fonte: ANATEL abril/2025)</li>
+        </ul>
+      </p>
+    </section>
+  )
+  
+}
+
+export default SobreNos;
