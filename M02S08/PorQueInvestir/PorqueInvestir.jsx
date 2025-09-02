0b213ca4dad88a0e8beb25e01554daef08934fe0
Douglas Graça <159181176+DougBrando@users.noreply.github.com>
2025-09-02 11:31:49 -0300

    Add files via upload

---
+++ M02S08/PorQueInvestir/PorqueInvestir.jsx
@@ -0,0 +1,33 @@
+import './PorqueInvestir.css'
+
+function PorqueInvestir() {
+  return (
+
+    <section id="#porqueinvestir">
+      <h2>Porque investir em energia solar?</h2>
+      <div className="box">
+        <div className="box-item">
+        <h3>Redução da conta</h3>
+        <p>Economize ate 90% na conta de energia eletrica.</p>
+         </div>
+         <div className="box-item">
+           <h3>Sustentabilidade</h3>
+           <p>Energia limpa e renovavel, ajudando o planeta.</p>
+           </div>
+
+           <div className="box-item">
+             <h3>Valorização do imovel</h3>
+             <p>Residencias com placa aumentam ate 10% no valor do mercado.</p>
+             </div>
+             <div className="box-item">
+               <h3>Retorno garantido</h3>
+               <p>Payback em 4-5 anos e economia mensal gararntida.</p>
+               </div>
+
+         </div>
+    </section>
+  )
+  
+}
+
+export default PorqueInvestir
