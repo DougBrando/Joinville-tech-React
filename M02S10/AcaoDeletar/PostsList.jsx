7745f3fd5a1ffb60394a358864cc8106b569c546
Douglas Graça <159181176+DougBrando@users.noreply.github.com>
2025-09-02 12:52:06 -0300

    Create PostsList.jsx

---
+++ M02S10/AcaoDeletar/PostsList.jsx
@@ -0,0 +1,30 @@
+import "./PostsList.css";
+import { PostContext } from "./PostsProvider";
+import Post from "./src/components/Post";
+import { useContext, useEffect, useState } from "react";
+
+export default function PostsList() {
+  const { posts, handleDelete } = useContext(PostContext);
+
+  return (
+    <main>
+      <h1>Posts</h1>
+      <p>
+        Atualmente, voce tem <strong>{posts.length} posts</strong> cadastrados
+      </p>
+      {posts.map((post, index) => (
+        <Post
+          key={index}
+          url={post.url}
+          titulo={post.titulo}
+          conteudo={post.conteudo}
+          tipo={post.tipo}
+          data={post.data}
+          // Função para deletar o post
+          index={index}
+          handleDelete={handleDelete}
+        />
+      ))}
+    </main>
+  );
+}
